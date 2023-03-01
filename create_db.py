8ec831eadc9d1e27928c1f3e11ddbf33f4ca5cfd
james <james.anthony.russell36@gmail.com>
2023-03-01 21:42:01 +1100

    need to fix the column not loading to the dashboard (price)

--- create_db.py
+++ create_db.py
@@ -6,29 +6,29 @@ print(connection.total_changes)
 
 cursor = connection.cursor()
 
-cursor.execute("""
-        CREATE TABLE IF NOT EXISTS stock (
-            id INTEGER PRIMARY KEY,
-            symbol TEXT NOT NULL UNIQUE,
-            name TEXT NOT NULL,
-            exchange TEXT NOT NULL
-        )""")
+# cursor.execute("""
+#         CREATE TABLE IF NOT EXISTS stock (
+#             id INTEGER PRIMARY KEY,
+#             symbol TEXT NOT NULL UNIQUE,
+#             name TEXT NOT NULL,
+#             exchange TEXT NOT NULL
+#         )""")
 
-cursor.execute("""
-        CREATE TABLE IF NOT EXISTS stock_price (
-            id INTEGER PRIMARY KEY,
-            stock_id INTEGER,
-            date NOT NULL,
-            open NOT NULL,
-            high NOT NULL,
-            low NOT NULL,
-            close NOT NULL,
-            volume NOT NULL,
-            sma_21,
-            sma_5,
-            rsi_13,
-            FOREIGN KEY (stock_id) REFERENCES stock (id)
-        )""")
+# cursor.execute("""
+#         CREATE TABLE IF NOT EXISTS stock_price (
+#             id INTEGER PRIMARY KEY,
+#             stock_id INTEGER,
+#             date NOT NULL,
+#             open NOT NULL,
+#             high NOT NULL,
+#             low NOT NULL,
+#             close NOT NULL,
+#             volume NOT NULL,
+#             sma_21,
+#             sma_5,
+#             rsi_13,
+#             FOREIGN KEY (stock_id) REFERENCES stock (id)
+#         )""")
 
 cursor.execute("""
         CREATE TABLE IF NOT EXISTS strategy (
